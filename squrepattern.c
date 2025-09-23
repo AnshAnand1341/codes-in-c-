af16b8da0b2d7ab46e8c4ebf709ff06bda7accde
AnshAnand1341 <manguanand086@gmail.com>
2025-09-23 09:42:50 +0530

    Add files via upload

---
+++ squrepattern.c
@@ -0,0 +1,14 @@
+// code to print square pattern 
+#include <stdio.h>
+
+int main() {
+   for(int i=1 ; i<=5 ; i++){
+       for(int j=1 ; j<=5 ;j++){
+           printf("*");
+           
+       }
+       printf("\n");
+       
+   }
+   return 0;
+}
